60f054036df06f190730db164ca4926c8651033d
preyedebo1996 <preyedebo@gmail.com>
2022-05-21 08:51:05 -0700

    git ignore main.c

---
+++ printf.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * _printf - produces output according to a format.
+ * @format: the format to be used for the output cases.
+ * Return: 0 on  success.
+ */
+int _printf(const char *format, ...)
+{
+
+}
