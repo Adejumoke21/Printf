54075db4f7978a7d35216184dba7b8d6ede98c47
preyedebo1996 <preyedebo@gmail.com>
2022-05-21 09:52:10 -0700

    prints just arguments

--- printf.c
+++ printf.c
@@ -3,10 +3,21 @@
 #include <stdlib.h>
 /**
  * _printf - produces output according to a format.
- * @format: the format to be used for the output cases.
- * Return: 0 on  success.
+ * @format: pointer to format to be used for the output cases.
+ * Return: the number of characters printed
+ *	(excluding the null byte used to end output to strings).
  */
 int _printf(const char *format, ...)
 {
+	/**
+	* to display the characters.
+	*/
+	int i;
 
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		_putchar(format[i]);
+	}
+
+	return (0);
 }
